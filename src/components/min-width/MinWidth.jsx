d0b7822b4f41f328634aab3dcc017664b916a84f
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-05-19 12:33:22 +0200

    Added Min width UI

---
+++ src/components/min-width/MinWidth.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import logo from '../../assets/logo.png'
+
+const MinWidth = () => {
+  return (
+    <div className="max-width-container">
+        <div className="wrapper">
+            <div className="container">
+                <img src={logo} alt='logo' className='center' />
+                <h1>Express store admin panel</h1>
+                <p>You can only view this application on a computer</p>
+                <p>Or in any device with a screen above 768 pixels.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default MinWidth
